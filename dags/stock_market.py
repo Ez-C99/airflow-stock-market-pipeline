aded8471f8caf7282bd1cdebbfb739dc4f71b45f
ezra_chamba <ezra.chamba@withintelligence.com>
2024-07-08 20:13:29 +0100

    working api availability task

--- dags/stock_market.py
+++ dags/stock_market.py
@@ -1,8 +1,8 @@
 from airflow.decorators import dag, task
 from airflow.hooks.base import BaseHook
-from airflow.sensors.base import PokeReturnValue
 from datetime import datetime
 import requests
+import logging
 
 @dag(
     description='The stock market DAG',
@@ -18,23 +18,19 @@ def stock_market():
         timeout=300,            # 5 minute timeout
         mode='poke'
     )
-    def is_api_available() -> PokeReturnValue:
+    def is_api_available() -> bool:
         api = BaseHook.get_connection('stock_api')
         url = f"{api.host}/query?function=TIME_SERIES_DAILY&symbol={api.extra_dejson['symbol']}&apikey={api.extra_dejson['api_key']}"
         response = requests.get(url)
         data = response.json()
 
-        # Check if the response contains the expected data
-        condition = "Time Series (Daily)" in data and data["Time Series (Daily)"]
-        return PokeReturnValue(is_done=condition, xcom_value=data)
-    
-    # Assuming you want to save the data fetched to Minio or process it further
-    @task()
-    def process_data(api_data):
-        # Logic to process data or save to Minio
-        pass
+        # Log the response data for debugging purposes
+        # logging.info(f"API response data: {data}")
 
-    api_data = is_api_available()
-    process_data(api_data)
+        # Check if the response contains the expected data
+        condition = "Time Series (Daily)" in data and bool(data["Time Series (Daily)"])
+        return condition
+
+    is_api_available()
 
 stock_market()
